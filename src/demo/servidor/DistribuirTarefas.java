500a1334b14cd5479b9824af0ae496ec1716b217
alexfabianojr <alexfabianojr@gmail.com>
2020-08-13 10:02:41 -0300

    aula 6

--- src/demo/servidor/DistribuirTarefas.java
+++ src/demo/servidor/DistribuirTarefas.java
@@ -1,6 +1,7 @@
 package demo.servidor;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -25,9 +26,24 @@ public class DistribuirTarefas implements Runnable {
     @Override
     public void run() {
         System.out.println("Distribuindo Tarefas para " + socket);
-        try (Scanner sc = new Scanner(socket.getInputStream())) {
+        try (Scanner sc = new Scanner(socket.getInputStream());
+             PrintStream saidaCliente = new PrintStream(socket.getOutputStream())) {
             while (sc.hasNextLine()) {
                 String comando = sc.nextLine();
+                System.out.printf("Comando recebido %s", comando);
+                switch (comando) {
+                    case "c1" -> {
+                        saidaCliente.println("Confirmação comando c1");
+                    }
+                    case "c2" -> {
+                        saidaCliente.println("Confirmação comando c2");
+                    }
+                    default -> {
+                        saidaCliente.println("Comando nao encontrado");
+                    }
+                }
+
+
                 System.out.println(comando);
             }
         } catch (IOException e) {
