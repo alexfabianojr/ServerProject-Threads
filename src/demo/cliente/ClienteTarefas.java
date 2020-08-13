ec24f3b3414d542a4330280437731d3600557ca5
alexfabianojr <alexfabianojr@gmail.com>
2020-08-13 02:49:27 -0300

    aula 5

--- src/demo/cliente/ClienteTarefas.java
+++ src/demo/cliente/ClienteTarefas.java
@@ -12,11 +12,49 @@ public class ClienteTarefas {
         try (Socket socket = new Socket("localhost", 12345);
              PrintStream saida = new PrintStream(socket.getOutputStream());
              Scanner sc = new Scanner(System.in);) {
+
             System.out.println("Conexao estabelecida");
-            saida.println("comando 1");
-            sc.nextLine();
-        } catch (IOException e) {
+
+            Thread threadEnviaComando = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (sc.hasNextLine()) {
+                        String linha = sc.nextLine();
+                        if (linha.trim().equals("")) {
+                            break;
+                        }
+                        saida.println(linha);
+                    }
+                }
+            });
+
+            Thread threadRecebeRespostaDoServidor = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println("Recebendo dados do servidor");
+                        Scanner respostaServidor = new Scanner(socket.getInputStream());
+                        while (respostaServidor.hasNextLine()) {
+                            String linha = respostaServidor.nextLine();
+                            System.out.println(linha);
+                        }
+                    } catch (IOException e) {
+                        System.out.println(Arrays.toString(e.getStackTrace()));
+                        throw new RuntimeException();
+                    }
+                }
+            });
+
+            threadEnviaComando.start();
+            threadRecebeRespostaDoServidor.start();
+
+            threadEnviaComando.join();
+
+            System.out.println("Fechando socket do cliente");
+
+        } catch (IOException | InterruptedException e) {
             System.out.println(Arrays.toString(e.getStackTrace()));
+            throw new RuntimeException();
         }
     }
 }
