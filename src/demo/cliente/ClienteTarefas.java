7aa16c0efeccc01c95a638eb2475c7fc272bc2b3
alexfabianojr <alexfabianojr@gmail.com>
2020-08-13 01:53:01 -0300

    aula 4

--- src/demo/cliente/ClienteTarefas.java
+++ src/demo/cliente/ClienteTarefas.java
@@ -4,18 +4,19 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ClienteTarefas {
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 12345);
-        System.out.println("Conexao estabelecida");
-        PrintStream saida = new PrintStream(socket.getOutputStream());
-        saida.println("comando 1");
-        Scanner sc = new Scanner(System.in);
-        sc.nextLine();
-        sc.close();
-        saida.close();
-        socket.close();
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 12345);
+             PrintStream saida = new PrintStream(socket.getOutputStream());
+             Scanner sc = new Scanner(System.in);) {
+            System.out.println("Conexao estabelecida");
+            saida.println("comando 1");
+            sc.nextLine();
+        } catch (IOException e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
     }
 }
--- src/demo/servidor/ServidorTarefas.java
+++ src/demo/servidor/ServidorTarefas.java
@@ -3,17 +3,19 @@ package demo.servidor;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServidorTarefas {
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("----Iniciando o servidor----");
         ServerSocket servidor = new ServerSocket(12345);
+        ExecutorService threadPool = Executors.newCachedThreadPool();
         while (true) {
             Socket socketAccept = servidor.accept();
             System.out.println("Aceitando o novo cliente " + socketAccept.getPort());
             DistribuirTarefas tarefas = new DistribuirTarefas(socketAccept);
-            Thread thread = new Thread(tarefas);
-            thread.start();
+            threadPool.execute(tarefas);
             Thread.sleep(20000);
         }
     }
